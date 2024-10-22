04f8fb2d5746f2c69bcc94d4d84bba8e22baebee
su1jun <qaxsd530@gmail.com>
2024-10-22 21:16:50 +0900

    한권으로 끝내는 nodejs완강

---
+++ Chap18-19/lib/auth.js
@@ -0,0 +1,118 @@
+const passport = require('passport')
+const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const db = require('../db')
+
+passport.serializeUser((user, done) => done(null, user._id))
+passport.deserializeUser((id, done) => {
+  db.getUserById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err, null))
+})
+
+function handleRedirect(req, res, options, code = 303) {
+  // the default way to specify where to rediretc to after successful redirect is
+  // req.session.authRedirect; see "customerOnly" and "employeeOnly" middlware for
+  // an example.  req.query.redirect is also provided for testing & future expansion.
+  const redirectUrl = req.session.authRedirect || req.query.redirect || options.successRedirect
+  delete req.session.authRedirect // harmless if it doesn't exist
+  res.redirect(code, redirectUrl)
+}
+
+module.exports = (app, options) => {
+
+	// if success and failure redirects aren't specified,
+	// set some reasonable defaults
+	if(!options.successRedirect)
+		options.successRedirect = '/account'
+	if(!options.failureRedirect)
+		options.failureRedirect = '/login'
+
+	return {
+    init: function() {
+      const config = options.providers
+
+      // configure Facebook strategy
+      passport.use(new FacebookStrategy({
+        clientID: config.facebook.appId,
+        clientSecret: config.facebook.appSecret,
+        callbackURL: (options.baseUrl || '') + '/auth/facebook/callback',
+      }, (accessToken, refreshToken, profile, done) => {
+        const authId = 'facebook:' + profile.id
+        db.getUserByAuthId(authId)
+          .then(user => {
+            if(user) return done(null, user)
+            db.addUser({
+              authId: authId,
+              name: profile.displayName,
+              created: new Date(),
+              role: 'customer',
+            })
+              .then(user => done(null, user))
+              .catch(err => done(err, null))
+          })
+          .catch(err => {
+            console.log('whoops, there was an error: ', err.message)
+            if(err) return done(err, null);
+          })
+      }))
+
+      // configure Google strategy
+      passport.use(new GoogleStrategy({
+        clientID: config.google.clientID,
+        clientSecret: config.google.clientSecret,
+        callbackURL: (options.baseUrl || '') + '/auth/google/callback',
+      }, (token, tokenSecret, profile, done) => {
+        const authId = 'google:' + profile.id
+        db.getUserByAuthId(authId)
+          .then(user => {
+            if(user) return done(null, user)
+            db.addUser({
+              authId: authId,
+              name: profile.displayName,
+              created: new Date(),
+              role: 'customer',
+            })
+              .then(user => done(null, user))
+              .catch(err => done(err, null))
+          })
+          .catch(err => {
+            console.log('whoops, there was an error: ', err.message)
+            if(err) return done(err, null);
+          })
+      }))
+
+      app.use(passport.initialize())
+      app.use(passport.session())
+    },
+    registerRoutes: () => {
+      // register Facebook routes
+      app.get('/auth/facebook', (req, res, next) => {
+        if(req.query.redirect) req.session.authRedirect = req.query.redirect
+        passport.authenticate('facebook')(req, res, next)
+      })
+      app.get('/auth/facebook/callback', passport.authenticate('facebook',
+        { failureRedirect: options.failureRedirect }),
+        (req, res) => {
+          console.log('successful /auth/facebook/callback')
+          // we only get here on successful authentication
+          handleRedirect(req, res, options)
+        }
+      )
+      // register Google routes
+      app.get('/auth/google', (req, res, next) => {
+        if(req.query.redirect) req.session.authRedirect = req.query.redirect
+        passport.authenticate('google', { scope: ['profile'] })(req, res, next)
+      })
+      app.get('/auth/google/callback', passport.authenticate('google',
+        { failureRedirect: options.failureRedirect }),
+        (req, res) => {
+          console.log('successful /auth/google/callback')
+          // we only get here on successful authentication
+          handleRedirect(req, res, options)
+        }
+      )
+    },
+	}
+}
